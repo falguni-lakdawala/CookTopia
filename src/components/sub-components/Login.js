3ee8069bc3211cd759c0712b3ee13d2e7062a30e
Maneesh <thouti.maneesh43@gmail.com>
2021-06-25 11:38:42 -0700

    404 page added

--- src/components/sub-components/Login.js
+++ src/components/sub-components/Login.js
@@ -7,15 +7,28 @@ import { useHistory}  from 'react-router-dom'
 const Login = () => {
     const history=useHistory();
     return (
-        <div className="login">
-            <h1>Login</h1>
-            <Input type={"email"} placeholder={"Enter your email"}/>
-            <Input type={"password"} placeholder={"Enter your password"}/>
-            <Link to ={{pathname:'/forgot'}} >forgot password</Link>
-            <button type="button" onClick={()=>{history.push('/home')}}>Login</button>
-            <Button text={"Signup"}/>
+      <div className="login">
+        <h1>Login</h1>
+        <div className="logininputs">
+          <div className="logininputs1">
+            <Input type={"email"} placeholder={"Enter your email"} />
+            <Input type={"password"} placeholder={"Enter your password"} />
+            <Link to={{ pathname: "/forgot" }}>forgot password</Link>
+          </div>
+          <div className="logininputs2">
+            <button
+              type="button"
+              onClick={() => {
+                history.push("/home");
+              }}
+            >
+              Login
+            </button>
+            <Button text={"Signup"} />
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Login
