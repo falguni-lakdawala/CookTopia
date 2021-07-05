b3c87e366905fceb1cb5ec43032db4867a61acd7
Maneesh <thouti.maneesh43@gmail.com>
2021-07-05 15:39:28 -0700

    Data visualization added

--- src/components/sub-components/Recipes/RecipeContentMainInstructions.js
+++ src/components/sub-components/Recipes/RecipeContentMainInstructions.js
@@ -1,79 +1,110 @@
 import recipeLikeIcon from '../../../assets/icons/recipe-like.svg';
 import recipeDislikeIcon from '../../../assets/icons/recipe-dislike.svg';
+import MyResponsivePie from '../Visuaization/MyResponsiveChart';
+
 
 const RecipeContentMainInstructions = ({ recipeData }) => {
 	console.log(recipeData)
 	const nutritionData = recipeData.nutrition.nutrients
-	const recipeContentMainInstructionsData = {
-		totalCalories: '2200 Kcal',
-		instructionsTextData: [
-			{
-				id: 1,
-				heading: 'Heat pasta water',
-				text: 'Rinse the gigantes or lima beans and soak in several inches of water for 8 hours or overnight. Drain and rinse.'
-			},
-			{
-				id: 2,
-				heading: 'Beat eggs and half of the cheese',
-				text: 'In a small bowl, beat the eggs and mix in about half of the cheese.'
-			},
-			{
-				id: 3,
-				heading: 'Cook paste',
-				text: 'Once the water has reached a rolling boil, add the dry pasta, and cook, uncovered, at a rolling boil.'
-			},
-		]
-	}
+	console.log(nutritionData)
+
+
+	const data = {
+    data: [
+      {
+        id: "Carbohydrates",
+        label: "Carbohydrates",
+        value: nutritionData[4].amount,
+        color: "hsl(129, 70%, 50%)",
+      },
+      {
+        id: "Sugar",
+        label: "Sugar",
+        value: nutritionData[5].amount,
+        color: "hsl(221, 70%, 50%)",
+      },
+      {
+        id: "Calcium",
+        label: "Calcium",
+        value: nutritionData[21].amount,
+        color: "hsl(1, 70%, 50%)",
+      },
+      {
+        id: "Folate",
+        label: "Folate",
+        value: nutritionData[13].amount,
+        color: "hsl(251, 70%, 50%)",
+      },
+      {
+        id: "Vitamin C",
+        label: "Vitamin C",
+        value: nutritionData[9].amount,
+        color: "hsl(299, 70%, 50%)",
+      },
+    ],
+  };
 
 	return (
-		<>
-			<div className="recipe-instructions-cont">
-				<div className="instructions-graph-cont">
-					<div className="instructions-cont-graph">
-						<div className="dummy"></div>
-					</div>
-				</div>
-				<div className="instructions-total-calories-cont">
-					<div className="instructions-total-calories-box">
-						<div className="label">Total Calories</div>
-						<div className="instructions-total-calories-text">{nutritionData[0].amount + " " + nutritionData[0].unit}</div>
-					</div>
-				</div>
-				<div className="recipe-instructions-text-cont">
-					<ol>
-						{
-							recipeContentMainInstructionsData.instructionsTextData.map(
-								(data, index) =>
-									<li key={index}>
-										<div className="instructions-text-heading">{data.heading}</div>
-										<div className="instructions-text-text">{data.text}</div>
-									</li>
-							)
-						}
-					</ol>
-				</div>
+    <>
+      <div className="recipe-instructions-cont">
+        <div className="instructions-graph-cont">
+          <div className="instructions-cont-graph">
+            <div className="dummy">
+
+<MyResponsivePie data={data.data}/>
+
 			</div>
-			<div className="recipe-like-dislike-cont">
-				<div className="like-cont">
-					<button role="button" aria-label="like" title="like" type="button" className="recipe-like">
-						<img src={recipeLikeIcon} alt="recipe like button" />
-					</button>
-					<div className="like-text">
-						{recipeData.aggregateLikes}
-					</div>
-				</div>
-				<div className="dislike-cont">
-					<button role="button" aria-label="dislike" title="dislike" type="button" className="recipe-dislike">
-						<img src={recipeDislikeIcon} alt="recipe like button" />
-					</button>
-					<div className="dislike-text">
-						{/* {recipeData.aggregateDislikes} */}
-						5
-					</div>
-				</div>
-			</div>
-		</>
-	)
+          </div>
+        </div>
+        <div className="instructions-total-calories-cont">
+          <div className="instructions-total-calories-box">
+            <div className="label">Total Calories</div>
+            <div className="instructions-total-calories-text">
+              {nutritionData[0].amount + " " + nutritionData[0].unit}
+            </div>
+          </div>
+        </div>
+        <div className="recipe-instructions-text-cont">
+          <ol>
+            {recipeData.analyzedInstructions[0].steps.map((data, index) => (
+              <li key={data.toString()+index}>
+                {/* <div className="instructions-text-heading">{data.heading}</div> */}
+                <div className="instructions-text-text">{data.step}</div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+      <div className="recipe-like-dislike-cont">
+        <div className="like-cont">
+          <button
+            role="button"
+            aria-label="like"
+            title="like"
+            type="button"
+            className="recipe-like"
+          >
+            <img src={recipeLikeIcon} alt="recipe like button" />
+          </button>
+          <div className="like-text">{recipeData.aggregateLikes}</div>
+        </div>
+        <div className="dislike-cont">
+          <button
+            role="button"
+            aria-label="dislike"
+            title="dislike"
+            type="button"
+            className="recipe-dislike"
+          >
+            <img src={recipeDislikeIcon} alt="recipe like button" />
+          </button>
+          <div className="dislike-text">
+            {/* {recipeData.aggregateDislikes} */}5
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default RecipeContentMainInstructions
