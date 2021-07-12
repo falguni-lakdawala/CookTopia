4712f4592787ac2dd18f01b00a7e7f150dfba26e
Abhijeet Singh <abjt14@pop-os.localdomain>
2021-07-12 15:43:02 -0700

    added basic styling to shopping list page

--- src/components/sub-components/ShoppingCard.js
+++ src/components/sub-components/ShoppingCard.js
@@ -1,69 +1,46 @@
+const ShoppingCard = ({ recipes, recipeClassName, ingredientList }) => {
+	return (
+		<div className={recipeClassName}>
+			{recipes.map((data, index) => {
+				return (
+					<div
+						key={index + data.title}
+						className="shopping-list-listing"
+					>
+						<div className="img-and-remove-list-cont">
+							<div className="img-title-cont">
+								<img src={data.src} alt={data.alt} />
+								<div className="title">{data.title}</div>
+							</div>
+							<div className="remove-list-cont">
+								<button type="button" className="remove-list-btn">
+									remove list
+								</button>
+							</div>
+						</div>
+						<div className="ingredients-cont">
+							{data.ingredients.map((dataInner, index) => {
+								return (
+									<div
+										key={index + dataInner.text}
+										className="ingredient-cont"
+									>
+										<div className="checkbox">
+											<input
+												type="checkbox"
+												defaultChecked={dataInner.selected}
+											/>
+										</div>
+										<div className="text">{dataInner.text}</div>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
-import React from 'react'
-import Images from '../composable-components/Images'
-
-const ShoppingCard = ({recipes, recipeClassName, ingredientList }) => {
-return(
-    <div className={recipeClassName}>
-    {
-        recipes.map(data=>{
-            return(
-                <div className="shopping_card" key={data.src}>
-                    <div>
-                        <Images src={data.src} alt={data.alt} />
-                        <h4 role="heading">{data.title}</h4>
-                        <label>Reset List</label>
-                    </div>
-                    
-
-                    <div className="ingredientlist">
-                        <div>
-                            <input aria-labelledby="apple" type="checkbox" id="apple" name="apple" value="Apple" />
-                            <label htmlFor="apple"> Apple</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>5 Pcs</label><br />
-                        </div>
-                        <div>
-                            <input aria-labelledby="sugar" type="checkbox" id="sugar" name="sugar" value="Sugar" />
-                            <label htmlFor="sugar"> Sugar</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>11 g</label><br />
-                        </div>
-                        
-                        <div>
-                            <input aria-labelledby="raddish" type="checkbox" id="raddish" name="raddish" value="Raddish" />
-                            <label htmlFor="raddish"> Raddish</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>2 Pcs</label><br />
-                        </div>
-                        
-                        <div>
-                            <input aria-labelledby="tomato" type="checkbox" id="tomato" name="tomato" value="Tomato" />
-                            <label htmlFor="tomato"> Tomato</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>3 Pcs</label>
-                        </div>
-
-                        <div>
-                            <input type="checkbox" aria-labelledby="apple" id="apple" name="apple" value="Apple" />
-                            <label htmlFor="apple"> Apple</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>5 Pcs</label><br />
-                        </div>
-                        <div>
-                            <input type="checkbox" id="sugar" name="sugar" value="Sugar" />
-                            <label htmlFor="sugar"> Sugar</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>11 g</label><br />
-                        </div>
-                        
-                        <div>
-                            <input type="checkbox" id="raddish" name="raddish" value="Raddish" />
-                            <label htmlFor="raddish"> Raddish</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>2 Pcs</label><br />
-                        </div>
-                        
-                        <div>
-                            <input type="checkbox" id="tomato" name="tomato" value="Tomato" />
-                            <label htmlFor="tomato"> Tomato</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>3 Pcs</label>
-                        </div>
-                        
-                    </div> 
-                        <a className="more_ingredient" href="#">More Ingredients</a>
-                </div>
-            )
-        })
-    }
-    </div>
-)
-
-}
-
-export default ShoppingCard
+export default ShoppingCard;
