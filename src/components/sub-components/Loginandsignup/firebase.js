bcd6deea673a0e31f543bc3b7eb73a11416e482e
falguni-lakdawala <falguni.lakdawala@gmail.com>
2021-07-14 19:34:15 -0700

    google authentication using firebase #91
    google authentication using firebase #91

---
+++ src/components/sub-components/Loginandsignup/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZMQTshUKvJhewu6ElLU-X5YOGSwJf7aM",
+    authDomain: "done-ninja.firebaseapp.com",
+    databaseURL: "https://done-ninja-default-rtdb.firebaseio.com",
+    projectId: "done-ninja",
+    storageBucket: "done-ninja.appspot.com",
+    messagingSenderId: "361469444727",
+    appId: "1:361469444727:web:9ffa642d9916e347f93c72",
+    measurementId: "G-LLDCSBDFNF"
+};
+
+let instance;
+
+export default function getFirebase() {
+    if (typeof window !== "undefined") {
+        if (instance) return instance
+        instance = firebase.initializeApp(firebaseConfig);
+        return instance
+    }
+
+    return null
+}
