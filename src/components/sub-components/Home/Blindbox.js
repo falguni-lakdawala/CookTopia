45ea274ee9bf0468b6561305afd588eec1b1905b
Abhijeet Singh <abjt14@pop-os.localdomain>
2021-07-22 19:44:54 -0700

    several changes -> check git issues

--- src/components/sub-components/Home/Blindbox.js
+++ src/components/sub-components/Home/Blindbox.js
@@ -6,33 +6,36 @@ const Blindbox = ({ style }) => {
 		<div className="blindbox-hero-cont">
 			<div className="max-width-cont">
 				<div className="blindbox-hero-text">
-					Cooktopia provides a variety of recipes, <br />
-					nutrition data, and nearby grocery shops. <br />
+					<strong>Cooktopia</strong> provides a variety of recipes,
+					nutrition data, and nearby grocery shops.
 					<br />
-					Anyone could enjoy well-being life <br />
+					<br />
+					Anyone could enjoy well-being life
 					based on well-balanced diet at home.
 				</div>
 				<div className="blindbox-hero-image-btn">
-					<div className="blindbox-hero-image-cont">
-						<img
-							src={blindboxHeroImage}
-							alt="hero image"
-							role="img"
-							aria-label="box image"
-						/>
-					</div>
-					<div className="blindbox-hero-btn-cont">
-						<Link to="/recipes">
-							<button
-								title="browse recipes"
-								type="button"
-								className="blindbox-hero-btn"
-								role="button"
-								aria-label="button"
-							>
-								Browse Recipes
-							</button>
-						</Link>
+					<div>
+						<div className="blindbox-hero-image-cont">
+							<img
+								src={blindboxHeroImage}
+								alt="hero image"
+								role="img"
+								aria-label="box image"
+							/>
+						</div>
+						<div className="blindbox-hero-btn-cont">
+							<Link to="/recipes">
+								<button
+									title="browse recipes"
+									type="button"
+									className="blindbox-hero-btn"
+									role="button"
+									aria-label="button"
+								>
+									Browse Recipes
+								</button>
+							</Link>
+						</div>
 					</div>
 				</div>
 			</div>
